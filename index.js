32852a9d5effd113740009d2ef28b8b5cc0d2d2c
ome chukwuemeka <omeco4christ@gmail.com>
2024-03-23 14:07:05 +0100

    second commit
    We added functions

--- index.js
+++ index.js
@@ -15,3 +15,65 @@ let n = c/k;
 let e = n - m;
 
 console.log(e);
+
+function b(a){
+  let b = a*a;
+  return b;
+}
+ba = b(a);
+
+function c(t){
+  let c = t*t*t;
+  return c;
+}
+ca = c(t);
+
+function d(ba,ca){
+  let d = ba + ca;
+  return d;
+}
+da = d(ba,ca);
+
+function p(da){
+  let p = da*da;
+  return p;
+}
+pa = p(da);
+
+function h(pa){
+  let h = 4*pa;
+  return h;
+}
+ha = h(pa);
+
+function j(g,f){
+  let j = g + f;
+  return j;
+}
+ja = j(g,f);
+
+function k(ja){
+  let k = 3*ja;
+  return k;
+}
+ka = k(ja);
+
+function m(ha,ka){
+  let m = ha/ka;
+  return m;
+}
+ma = m(ha,ka);
+
+function n(ca,ka){
+  let n = ca/ka;
+  return n;
+}
+na = n(ca,ka);
+
+function e(na,ma){
+  let e = na - ma;
+  return e;
+}
+result = e(na,ma);
+
+console.log(result);
