f11f4ad76ee211fc9e5758dd32e7fc802c5521bb
ome chukwuemeka <omeco4christ@gmail.com>
2024-03-23 13:07:34 +0100

    Create index.js
    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+let t = 1;
+let f = 2;
+let g = 3;
+let a = 4;
+
+let b = a*a; 
+let c = t*t*t;
+let d = b + c;
+let p = d*d;
+let h = 4*p;
+let j = g + f;
+let k = 3*j;
+let m = h/k;
+let n = c/k;
+let e = n - m;
+
+console.log(e);
